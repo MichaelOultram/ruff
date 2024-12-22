pexip/os-ruff

allow human-friendly rule names in noqa directives

crates/ruff_linter/src/checkers/noqa.rs
@@ -12,7 +12,7 @@ use ruff_text_size::Ranged;
 
 use crate::fix::edits::delete_comment;
 use crate::noqa::{
-    Code, Directive, FileExemption, FileNoqaDirectives, NoqaDirectives, NoqaMapping,
+    Directive, FileExemption, FileNoqaDirectives, NoqaDirectives, NoqaIdentifier, NoqaMapping,
 };
 use crate::registry::{AsRule, Rule, RuleSet};
 use crate::rule_redirects::get_redirect_target;
@@ -134,32 +134,63 @@ pub(crate) fn check_noqa(
                     let mut valid_codes = vec![];
                     let mut seen_codes = FxHashSet::default();
                     let mut self_ignore = false;
-                    for original_code in directive.iter().map(Code::as_str) {
-                        let code = get_redirect_target(original_code).unwrap_or(original_code);
-                        if Rule::UnusedNOQA.noqa_code() == code {
-                            self_ignore = true;
-                            break;
+                    for directive in directive.iter() {
+                        let mut identifier = directive.identifier();
+
+                        let original_identifier = identifier.as_str();
+
+                        match identifier {
+                            NoqaIdentifier::Code(original_code) => {
+                                let code =
+                                    get_redirect_target(original_code).unwrap_or(original_code);
+                                identifier = NoqaIdentifier::Code(code);
+
+                                if Rule::UnusedNOQA.noqa_code() == code {
+                                    self_ignore = true;
+                                    break;
+                                }
+                            }
+                            NoqaIdentifier::Name(name) => {
+                                if Rule::UnusedNOQA.as_ref() == name {
+                                    self_ignore = true;
+                                    break;
+                                }
+                            }
                         }
 
-                        if !seen_codes.insert(original_code) {
-                            duplicated_codes.push(original_code);
-                        } else if line.matches.iter().any(|match_| *match_ == code)
-                            || settings
+                        let rule_result = match identifier {
+                            NoqaIdentifier::Code(code) => Rule::from_code(code),
+                            NoqaIdentifier::Name(name) => Rule::from_name(name),
+                        };
+
+                        if !seen_codes.insert(original_identifier) {
+                            duplicated_codes.push(original_identifier);
+                            continue;
+                        }
+
+                        let Ok(rule) = rule_result else {
+                            if settings
                                 .external
                                 .iter()
-                                .any(|external| code.starts_with(external))
-                        {
-                            valid_codes.push(original_code);
-                        } else {
-                            if let Ok(rule) = Rule::from_code(code) {
-                                if settings.rules.enabled(rule) {
-                                    unmatched_codes.push(original_code);
-                                } else {
-                                    disabled_codes.push(original_code);
-                                }
+                                .any(|external| original_identifier.starts_with(external))
+                            {
+                                valid_codes.push(original_identifier);
                             } else {
-                                unknown_codes.push(original_code);
+                                unknown_codes.push(original_identifier);
                             }
+                            continue;
+                        };
+
+                        if line
+                            .matches
+                            .iter()
+                            .any(|match_| *match_ == rule.noqa_code())
+                        {
+                            valid_codes.push(original_identifier);
+                        } else if settings.rules.enabled(rule) {
+                            unmatched_codes.push(original_identifier);
+                        } else {
+                            disabled_codes.push(original_identifier);
                         }
                     }
 
@@ -202,7 +233,7 @@ pub(crate) fn check_noqa(
                             )));
                         } else {
                             diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
-                                format!("# noqa: {}", valid_codes.join(", ")),
+                                format!("# noqa: {}", valid_codes.iter().join(", ")),
                                 directive.range(),
                             )));
                         }

crates/ruff_linter/src/rules/ruff/rules/redirected_noqa.rs
@@ -2,7 +2,7 @@ use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_text_size::Ranged;
 
-use crate::noqa::{Directive, NoqaDirectives};
+use crate::noqa::{Directive, NoqaDirectives, NoqaIdentifier};
 use crate::rule_redirects::get_redirect_target;
 
 /// ## What it does
@@ -49,18 +49,22 @@ pub(crate) fn redirected_noqa(diagnostics: &mut Vec<Diagnostic>, noqa_directives
             continue;
         };
 
-        for code in directive.iter() {
-            if let Some(redirected) = get_redirect_target(code.as_str()) {
+        for rule_ident in directive.iter() {
+            let NoqaIdentifier::Code(code) = rule_ident.identifier() else {
+                continue;
+            };
+
+            if let Some(redirected) = get_redirect_target(code) {
                 let mut diagnostic = Diagnostic::new(
                     RedirectedNOQA {
                         original: code.to_string(),
                         target: redirected.to_string(),
                     },
-                    code.range(),
+                    rule_ident.range(),
                 );
                 diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
                     redirected.to_string(),
-                    code.range(),
+                    rule_ident.range(),
                 )));
                 diagnostics.push(diagnostic);
             }

MichaelOultram/ruff

crates/ruff_linter/src/checkers/noqa.rs
@@ -11,7 +11,7 @@ use ruff_text_size::Ranged;
 
 use crate::fix::edits::delete_comment;
 use crate::noqa::{
-    Code, Directive, FileExemption, FileNoqaDirectives, NoqaDirectives, NoqaMapping,
+    Directive, FileExemption, FileNoqaDirectives, NoqaDirectives, NoqaIdentifier, NoqaMapping,
 };
 use crate::registry::{AsRule, Rule, RuleSet};
 use crate::rule_redirects::get_redirect_target;
@@ -129,32 +129,49 @@ pub(crate) fn check_noqa(
                     let mut valid_codes = vec![];
                     let mut seen_codes = FxHashSet::default();
                     let mut self_ignore = false;
-                    for original_code in directive.iter().map(Code::as_str) {
-                        let code = get_redirect_target(original_code).unwrap_or(original_code);
-                        if Rule::UnusedNOQA.noqa_code() == code {
+                    for directive in directive.iter() {
+                        let original_identifier = directive.as_str();
+                        let rule_result = match directive.identifier() {
+                            NoqaIdentifier::Code(original_code) => {
+                                let code =
+                                    get_redirect_target(original_code).unwrap_or(original_code);
+                                Rule::from_code(code)
+                            }
+                            NoqaIdentifier::Name(name) => Rule::from_name(name),
+                        };
+                        if let Ok(Rule::UnusedNOQA) = rule_result {
                             self_ignore = true;
                             break;
                         }
 
-                        if !seen_codes.insert(original_code) {
-                            duplicated_codes.push(original_code);
-                        } else if line.matches.iter().any(|match_| *match_ == code)
-                            || settings
+                        if !seen_codes.insert(original_identifier) {
+                            duplicated_codes.push(original_identifier);
+                            continue;
+                        }
+
+                        let Ok(rule) = rule_result else {
+                            if settings
                                 .external
                                 .iter()
-                                .any(|external| code.starts_with(external))
-                        {
-                            valid_codes.push(original_code);
-                        } else {
-                            if let Ok(rule) = Rule::from_code(code) {
-                                if settings.rules.enabled(rule) {
-                                    unmatched_codes.push(original_code);
-                                } else {
-                                    disabled_codes.push(original_code);
-                                }
+                                .any(|external| original_identifier.starts_with(external))
+                            {
+                                valid_codes.push(original_identifier);
                             } else {
-                                unknown_codes.push(original_code);
+                                unknown_codes.push(original_identifier);
                             }
+                            continue;
+                        };
+
+                        if line
+                            .matches
+                            .iter()
+                            .any(|match_| *match_ == rule.noqa_code())
+                        {
+                            valid_codes.push(original_identifier);
+                        } else if settings.rules.enabled(rule) {
+                            unmatched_codes.push(original_identifier);
+                        } else {
+                            disabled_codes.push(original_identifier);
                         }
                     }
 

crates/ruff_linter/src/rules/ruff/rules/redirected_noqa.rs
@@ -2,7 +2,9 @@ use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, ViolationMetadata};
 use ruff_text_size::Ranged;
 
-use crate::noqa::{Codes, Directive, FileNoqaDirectives, NoqaDirectives, ParsedFileExemption};
+use crate::noqa::{
+    Codes, Directive, FileNoqaDirectives, NoqaDirectives, NoqaIdentifier, ParsedFileExemption,
+};
 use crate::rule_redirects::get_redirect_target;
 
 /// ## What it does
@@ -69,18 +71,22 @@ pub(crate) fn redirected_file_noqa(
 
 /// Convert a sequence of [Codes] into [Diagnostic]s and append them to `diagnostics`.
 fn build_diagnostics(diagnostics: &mut Vec<Diagnostic>, codes: &Codes<'_>) {
-    for code in codes.iter() {
-        if let Some(redirected) = get_redirect_target(code.as_str()) {
+    for rule_ident in codes.iter() {
+        let NoqaIdentifier::Code(code) = rule_ident.identifier() else {
+            continue;
+        };
+
+        if let Some(redirected) = get_redirect_target(code) {
             let mut diagnostic = Diagnostic::new(
                 RedirectedNOQA {
                     original: code.to_string(),
                     target: redirected.to_string(),
                 },
-                code.range(),
+                rule_ident.range(),
             );
             diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
                 redirected.to_string(),
-                code.range(),
+                rule_ident.range(),
             )));
             diagnostics.push(diagnostic);
         }